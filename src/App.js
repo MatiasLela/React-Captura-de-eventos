d3662eba45468506581036a6c4534b3f84b844d4
MatiasLela <matiaslela0@gmail.com>
2023-03-28 01:50:52 -0300

    v1

--- src/App.js
+++ src/App.js
@@ -1,25 +1,19 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='App'>
+      <form onSubmit={mostrar}>
+        <input type="text" name="texto"/><br/>
+        <input type="submit" value="Mostrar"/>
+      </form>
     </div>
   );
+}
+
+function mostrar(e){
+  e.preventDefault();
+  alert(e.target.texto.value);
 }
 
 export default App;
